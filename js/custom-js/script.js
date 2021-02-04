f27297239ec59779da98e2b68e9ee62172bef3d6
Isaac Sokari <isaacsokari@gmail.com>
2021-02-04 19:17:15 +0100

    removed console.log

--- js/custom-js/script.js
+++ js/custom-js/script.js
@@ -1,12 +1,10 @@
 const dateInput = document.getElementById('inspectOn');
 
 const setInputAsDate = (e) => {
-  console.log(e.target.type);
   e.target.type = 'date';
 };
 
 const setInputAsText = (e) => {
-  console.log(e.target.type);
   e.target.type = 'text';
 };
 
